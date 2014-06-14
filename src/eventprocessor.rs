d18f4bb782b79a946fe86dfb092d500801db9b4a
Zachary Tong <zacharyjtong@gmail.com>
2014-06-14 01:25:52 -0400

    more

--- src/eventprocessor.rs
+++ src/eventprocessor.rs
@@ -3,7 +3,6 @@
 use sync::Arc;
 use std::sync::atomics::{AtomicInt, SeqCst, Release, Acquire};
 use waitstrategy::{WaitStrategy};
-use std::cmp::min;
 use paddedatomics::Padded64;
 
 pub struct EventProcessor {
@@ -22,32 +21,27 @@ impl EventProcessor {
 		}
 	}
 
-/*
+
 	pub fn start<T: WaitStrategy>(&self, f: ||) {
 		let wait_strategy: T = WaitStrategy::new();
 
+		let dep_eps = self.graph.get(self.token);
+		let mut deps: Vec<&Padded64> = Vec::with_capacity(dep_eps.len());
+		for ep in dep_eps.iter() {
+			deps.push((*self.cursors).get(*ep));
+		}
+		drop(dep_eps);
+
+		let cursor = (*self.cursors).get(self.token);
 		loop {
-			let next = self.get_sequence() + 1;
-			let available = wait_strategy.wait_for(next, self);
+			let next = cursor.load() + 1;
+			let available = wait_strategy.wait_for(next, &deps);
 
-			self.next(available);
+			// Get data from ring buff
+
+			//f(data);
 		}
 	}
+]
 
-	pub fn lowest_dep(&self) -> int {
-		let mut lowest = -1;
-		for ep in self.deps.iter() {
-			lowest = min(lowest, ep.get_sequence());
-		}
-		lowest
-	}
-
-	pub fn get_sequence(&self) -> int {
-		self.sequence.load(Acquire)
-	}
-
-	fn next(&self, next: int) {
-
-	}
-	*/
 }
--- src/waitstrategy.rs
+++ src/waitstrategy.rs
@@ -1,10 +1,20 @@
 
 use eventprocessor::EventProcessor;
+use paddedatomics::Padded64;
+use std::cmp::min;
 
 
 pub trait WaitStrategy {
 	fn new() -> Self;
-	fn wait_for(&self, sequence: int, ep: &EventProcessor) -> int;
+	fn wait_for(&self, sequence: int, ep: &Vec<&Padded64>) -> int;
+
+	fn lowest_dep(&self, deps: &Vec<&Padded64>) -> int {
+		let mut lowest = -1;
+		for cursor in deps.iter() {
+			lowest = min(lowest, cursor.load());
+		}
+		lowest
+	}
 }
 
 pub struct BusyWait;
@@ -14,12 +24,11 @@ impl WaitStrategy for BusyWait {
 		BusyWait
 	}
 
-	fn wait_for(&self, sequence: int, ep: &EventProcessor) -> int {
-		//let mut avail = ep.lowest_dep();
-		//while avail < sequence {
-			//avail = ep.lowest_dep();
-		//}
-		//avail
-		0
+	fn wait_for(&self, sequence: int, deps: &Vec<&Padded64>) -> int {
+		let mut avail: int  = self.lowest_dep(deps);
+		while avail < sequence {
+			avail = self.lowest_dep(deps);
+		}
+		avail
 	}
 }
